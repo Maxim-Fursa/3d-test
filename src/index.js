7f7be842bd75c150bd0f9bdbad746fc78fa5d815
Maxim-Fursa <maxonfurson@gmail.com>
2022-12-02 14:27:59 +1100

    change package.json

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from '@/App';
-import '@/styles.scss'
+import App from './App';
+import './styles.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
