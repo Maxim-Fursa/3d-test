8ef5d894b9cb68d2515bd938f030c907b25bf4b7
Maxim-Fursa <maxonfurson@gmail.com>
2022-12-02 13:58:59 +1100

    fix styles & clean code

--- src/App.js
+++ src/App.js
@@ -10,29 +10,29 @@ const Loader = ({ setLoad, animation }) => {
     const elem = React.useRef(null)
     const [arc_styles, arc_elem] = useSpring(() => ({
         loop: () => animation,
+        config: { tension: 280, friction: 60 },
         to: { transform: 'rotate(-150deg) skewX(30deg)' },
         from: { transform: 'rotate(150deg) skewX(30deg)' }
     }))
-    const [circel_styles, circle_elem] = useSpring(() => ({}))
+    const [circle_styles, circle_elem] = useSpring(() => ({}))
 
     React.useEffect(() => {
-        console.log(animation)
         if (!animation) {
             circle_elem({ height: '80vh', width: '80vh', top: 'calc(50% + 3rem)'})
             arc_elem({transform: 'rotate(150deg) skewX(30deg)', top: '-15px', left: '-5px', loop: false})
             setTimeout(() => { 
-                elem.current.classList.add('finished')
+                elem.current.classList.add('--finished')
                 setTimeout(() => {
                     setLoad(prev => ({...prev, inprocess: false}))
                 }, 300);
-            }, 1000);
+            }, 1300);
         }
     }, [animation])
 
     return (
         <div ref={elem} className="loader">
-            <animated.div style={circel_styles} className="circle">
-                <animated.div style={arc_styles} className="circle_arc"></animated.div>
+            <animated.div style={circle_styles} className="loader__circle">
+                <animated.div style={arc_styles} className="loader__arc"></animated.div>
             </animated.div>
         </div>
     )
@@ -46,9 +46,9 @@ function App() {
     const handleScroll = () => {
         if (parallax.current) {
             if (parallax.current.current >= window.innerHeight) {
-                document.querySelector('body').classList.add('black')
+                document.querySelector('body').classList.add('--black')
             } else {
-                document.querySelector('body').classList.remove('black')
+                document.querySelector('body').classList.remove('--black')
             }
         }
     }
@@ -58,7 +58,7 @@ function App() {
             setLoad(prev => ({...prev, animation: false}))
         }, 3000);
 
-        const container = document.querySelector('.my-class-name')
+        const container = document.querySelector('.parallax-wrapper__content')
         container.addEventListener('scroll', handleScroll)
         return () => {
             container.removeEventListener('scroll', handleScroll)
@@ -67,14 +67,12 @@ function App() {
 
 	return (
 		<div className="App">
-            {
-                load.inprocess && <Loader animation={load.animation} setLoad={setLoad} />
-            }
+            { load.inprocess && <Loader animation={load.animation} setLoad={setLoad} /> }
             <header>
                 <div className="container">
-                    <div className="in_header">
-                        <div className="logo">
-                            <div className="small_circle"></div>
+                    <div className="header__inner">
+                        <div className="logo header__logo">
+                            <div className="logo__circle"></div>
                             <p>.</p>
                         </div>
                         <nav>
@@ -90,30 +88,32 @@ function App() {
                     </div>
                 </div>
             </header>
-            <div className="parallax_wrapper">
-                <Parallax pages={3} style={{ top: '0', left: '0' }} ref={parallax} className='my-class-name'>
+            <div className="parallax-wrapper">
+                <Parallax pages={3} style={{ top: '0', left: '0' }} ref={parallax} className="parallax-wrapper__content">
                     <ParallaxLayer offset={0} speed={2.5} >
-                        <section className="dark_blue circle_section">
-                            <div className="little_circle circle_left">
-                                <div className="little_circle__content"> </div>
-                            </div>
-                            <div className="circle">
-                                <div className="circle_arc"></div>
-                                <div className="circle_text">
-                                    <h1>IPHONE.</h1>
-                                    <h3>New generation<br/>Big and bigger</h3>
+                        <section className="section-circle --dark_blue">
+                            <div className="section-circle__content">
+                                <div className="circle-item --small --small--left">
+                                    <div className="circle-item__content"> </div>
+                                </div>
+                                <div className="circle-item --big">
+                                    <div className="circle-item__arc"></div>
+                                    <div className="circle-item__text">
+                                        <h1>IPHONE.</h1>
+                                        <h3>New generation<br/>Big and bigger</h3>
+                                    </div>
+                                </div>
+                                <div className="circle-item --small --small--right">
+                                    <div className="circle-item__content"> </div>
                                 </div>
                             </div>
-                            <div className="little_circle circle_right">
-                                <div className="little_circle__content"> </div>
-                            </div>
-                            {/* <Spline className="circle_spline" scene="https://prod.spline.design/LuK3RzQcoI9ywFyg/scene.splinecode" /> */}
+                            <Spline className="circle_spline" scene="https://prod.spline.design/LuK3RzQcoI9ywFyg/scene.splinecode" />
                         </section>
                     </ParallaxLayer>
-                    <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#ff6d6d' }} />
+                    <ParallaxLayer offset={1} speed={1.5} style={{ backgroundColor: '#ff6d6d' }} />
                     <ParallaxLayer offset={1} speed={0.5}>
-                        <section className="pro_beyond_section">
-                            <div className="section_content">
+                        <section className="section-banner">
+                            <div className="section-banner__content">
                                 <h2>Pro.<br/>Beyond.</h2>
                                 <h3>Destroy your limits.</h3>
                                 <h4>From A$1,749 before trade-in<sup>1</sup></h4>
@@ -127,10 +127,10 @@ function App() {
                     <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#000' }} />
                     <ParallaxLayer offset={2} speed={0.5}>
                         <div className="container">
-                            <section className="black colors_section">
+                            <section className="section-colors">
                                 <h2>Designed for durability.</h2>
-                                <div className="section_content">
-                                    <ul className="list_images">
+                                <div className="section-colors__content">
+                                    <ul className="list-images">
                                         {
                                             [
                                                 'https://www.apple.com/au/iphone-14-pro/images/overview/gallery_deep_purple__du23dbfjl1km_large.jpg',
@@ -138,29 +138,29 @@ function App() {
                                                 'https://www.apple.com/au/iphone-14-pro/images/overview/gallery_silver__eph35go3eiy6_large.jpg',
                                                 'https://www.apple.com/au/iphone-14-pro/images/overview/gallery_space_black__ev5ncqabz7ma_large.jpg'
                                             ].map((el, idx) => 
-                                                <li key={el} className={colorsList === idx && "list_images__active"}>
+                                                <li key={el} className={colorsList === idx && "--active"}>
                                                     <img src={el} alt=""/>
                                                 </li>
                                             )
                                         }
                                     </ul>
                                     <div className="description">
-                                        <ul className="list_colors">
+                                        <ul className="list-colors">
                                             {
                                                 ["Deep Puprle", "Gold", "Silver", "Space Black"].map((el ,idx) => 
                                                     <li 
                                                         key={el} 
-                                                        className={colorsList === idx && "list_colors__active"}
+                                                        className={colorsList === idx && "--active"}
                                                         onClick={() => setColorsList(idx)}
                                                     >{el}</li>
                                                 )
                                             }
                                         </ul>
-                                        <div className="line_horizontal"></div>
+                                        <div className="description__line"></div>
                                         <p>
                                             With Ceramic Shield, tougher than any smartphone glass. Water resistance.2 Surgical-grade stainless steel. 6.1″ and 6.7″ display sizes.3 All in four Pro colours.
                                         </p>
-                                        <div className="bottom_content">
+                                        <div className="description__bottom">
                                             <FontAwesomeIcon icon={faLock} />
                                             <p>
                                                 iPhone is also designed from the ground up to protect your privacy and put you in control of what you share and who you share it with
@@ -168,8 +168,6 @@ function App() {
                                         </div>
                                     </div>
                                 </div>
-                                {/* <p>Second spline</p> */}
-                                {/* <Spline className="spline" scene="https://prod.spline.design/MXcS76HFKB6Sxszp/scene.splinecode" /> */}
                             </section>
                         </div>
                     </ParallaxLayer>
